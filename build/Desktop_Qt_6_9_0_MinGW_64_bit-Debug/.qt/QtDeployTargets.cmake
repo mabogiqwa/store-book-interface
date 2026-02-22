542a5d2bdcde9a5dacaf293615ea5aaa6fd80d69
echo404 <122114072+mabogiqwa@users.noreply.github.com>
2026-02-22 17:22:46 +0200

    deleted build folder

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Assignment2-Programming-Assignment_FILE C:/Users/realm/Documents/Assignment2-Programming-Assignment/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Assignment2-Programming-Assignment.exe)
-set(__QT_DEPLOY_TARGET_Assignment2-Programming-Assignment_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Assignment2-Programming-Assignment_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
